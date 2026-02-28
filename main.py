17c2db5cbfc8a6ac5ceb125b068e9709d0db3234
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-28 18:27:29 +0300

    Update main.py
    removed scanner error handling to right py

--- main.py
+++ main.py
@@ -4,11 +4,5 @@ def main()
     print(files)
     for f in files:
         print("Found:", f)
-    def scan_folder(path):
-        try:
-            return os.listdr(path)
-        except Exception as e;
-            print("Scan error:", e)
-            return []
 if __name__ == "__main__":
     main()
