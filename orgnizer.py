728bcdfd2ff2a0a98ea4750655304f041fce16e0
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-28 20:27:18 +0300

    Update orgnizer.py
    add basic organizer: move .txt files to documents folder

--- orgnizer.py
+++ orgnizer.py
@@ -10,3 +10,10 @@ def organize file(path):
     log-message("Organizer initialized")
     files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     log_message(f"Found {len(files)} files to organize")
+
+ for f in files:
+    if f.endswith(".txt"):
+        folder_path = os.path.join(path, "Documents")
+        os.makedirs(folder_path, exist_ok=True)
+        os.rename(os.path.join(path, f), os.path.join(folder_path, f))
+        log_message(f"Moved {f} to Documents")
