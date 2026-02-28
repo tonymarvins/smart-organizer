181c8f61686cf078934c1e598d4d560ed0441f8d
tonymarvins <tonymarvinscrewman@gmail.com>
2026-02-28 19:08:19 +0300

    Update main.py
    proper semi finished main.py

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 import scanner
 from utils import setup_logger, log_message
+
 def main():
     setup_logger()
-    log_message("smart Scanner started")
+    log_message("Smart Scanner started")
+
     files = scanner.scan_folder(".")
     print("Scanning current folder...\n")
 
     for f in files:
         print("Found:", f)
-        
-    # Filter only .py files for now
+
     py_files = scanner.filter_by_extension(files, ['.py'])
     print("\nPython files in folder:")
     for f in py_files:
-    print("->", f)
-
+        print("->", f)
+        
     count, size = scanner.file_statistics(files)
     print(f"\nTotal files: {count}, Total size: {size} bytes")
-   log_message(f"Python files: {py_files}")
-keyword = input("\nEnter keyword to search files (or leave empty): ")
-if keyword:
-    results = scanner.search_files(files, keyword)
-    print("\nSearch results:")
-    for r in results:
-        print("->", r)
-    log_message(f"Search results: {results}")
-  print("\nScan complete. See scan.log for full details.")
+
+    log_message(f"Python files: {py_files}")
+
+   
+    keyword = input("\nEnter keyword to search files (or leave empty): ")
+    if keyword:
+        results = scanner.search_files(files, keyword)
+        print("\nSearch results:")
+        for r in results:
+            print("->", r)
+        log_message(f"Search results: {results}")
+
+    print("\nScan complete. See scan.log for full details.")
+
+
 if __name__ == "__main__":
     main()
